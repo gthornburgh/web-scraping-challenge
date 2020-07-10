7d54918845870b2aceb25926960b235cdca1f62a
gthornburgh <grantthornburgh@gmail.com>
2020-07-10 08:46:51 -0400

    homework

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, jsonify
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+
+@app.route("/")
+def index():
+    mars_mongo = mongo.db.mars_mongo.find_one()
+    return render_template("index.html", mars = mars)
+
+@app.route("/scrape")
+def scrape():
+    
+    mars_mongo = mongo.db.mars_mongo
+    mars_data = scrape_mars_mongo.scrape()
+    mars.update({}, mars_data, upsert=True)
+    
+    return redirect("http://localhost:5000/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
